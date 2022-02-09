1878ff18019c6e9adbd711fc4aef327ca34db023
suhwan.kang <lionwithnomane@gmail.com>
2022-02-09 22:06:15 +0900

    remove redundent package

--- crawlingCode/kakaomapCrawling.py
+++ crawlingCode/kakaomapCrawling.py
@@ -1,16 +1,9 @@
-from tkinter import Menubutton
-from django.forms import CheckboxInput
-from numpy import getbufsize
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import time
 import re
-from posixpath import split
 import json
 from selenium.common.exceptions import NoSuchFrameException
 from selenium.common.exceptions import StaleElementReferenceException
--- crawlingCode/naverMenuCrawling.py
+++ crawlingCode/naverMenuCrawling.py
@@ -1,17 +1,6 @@
-from tkinter import Menubutton
-from numpy import getbufsize
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import time
 import re
-from posixpath import split
-import json
-from selenium.common.exceptions import NoSuchFrameException
 from selenium.common.exceptions import StaleElementReferenceException
 
 def naver(driver,menuGroup,placeName,actions,time):
