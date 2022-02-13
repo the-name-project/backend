8c0d77df8d9ab15e461c1dee551235219323fac0
nlpwantyou <king.god.ant@gmail.com>
2022-02-13 15:03:52 +0900

    update poll model

--- app/poll/model.py
+++ app/poll/model.py
@@ -10,9 +10,22 @@ class Poll(SQLModel, table=True):
     title: str
 
     contents: List["PollContent"] = Relationship(back_populates="poll")
-    vote: Optional["Vote"] = Relationship(back_populates="poll")
-    user_list: List["User"] = Field(foreign_key="user.id")
+    vote: List["Vote"] = Relationship(back_populates="poll")
+    user_list: List["User"] = Relationship(back_populates="poll")
 
+
+# 투표 내용
+class Poll_Content(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    description: str = Field(max_length=100, nullable=False)
+
+    store_id: Optional[int] = Field(default=None, foreign_key="store_info.id")
+    store: Optional["Store_Info"] = Relationship(back_populates="poll_content")
+
+    poll_id: Optional[int] = Field(foreign_key="poll.id", default=None)
+    poll: Optional["Poll"] = Relationship(back_populates="contents")
+
+    vote: List["Vote"] = Relationship(back_populates="poll_content")
 
 # 투표
 class Vote(SQLModel):
@@ -24,23 +37,7 @@ class Vote(SQLModel):
     content_id: Optional[str] = Field(foreign_key="poll_content.id", default=None)
     content: Optional["Poll_Content"] = Relationship(back_populates="vote")
 
-    user_id: Optional["User"] = Field(foreign_key="user.id")
-
-
-# 투표 내용
-class Poll_Content(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    content: str = Field(max_length=100, nullable=False)
-
-    store_id: Optional[int] = Field(default=None, foreign_key="store_info.id")
-    # store: Optional["Store"] = Relationship(back_populates="")
-
-    poll_id: Optional[int] = Field(foreign_key="poll.id", default=None)
-    poll: Optional["Poll"] = Relationship(back_populates="contents")
-
-    vote_id: Optional[int] = Field(foreign_key="vote.id", default=None)
-    # vote: List["Vote"] = Relationship(back_populates="")
-
+    user_id: List["User"] = Relationship(back_populates='vote')
 
 
 from sqlmodel import SQLModel, create_engine
