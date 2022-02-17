6d005151380b0b331e89bc0bca1db3053427212a
jnj3j3 <jnj22j33@gmail.com>
2022-02-18 00:45:51 +0900

    need pandas install

--- app/store/store_service.py
+++ app/store/store_service.py
@@ -1,8 +1,10 @@
-from sqlmodel import Field, Session,select,create_engine
+from typing import List
+from sqlmodel import Session,select,create_engine
 from fastapi.exceptions import HTTPException
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Query, status
 from app.store.model import Store_Info
 from app.store.menu.model import Menu
+import pandas as pd
 app = FastAPI()
 
 sqlite_file_name = "test_database.db"
@@ -13,14 +15,26 @@ engine = create_engine(sqlite_url, echo=True)
 session = Session(bind  = engine)
 # ?skip=0&limit=10
 @app.get('/stores',status_code=status.HTTP_200_OK)
-async def get_stores(skip: int = 0, limit: int = 10):
+async def get_stores(skip: int = 0, limit: int = 10,wheres:List[str]=Query(None)):
     Statement = select(Store_Info).offset(skip).limit(limit)
-    deafult = session.exec(Statement).all()
-
-    if deafult == None:
+    data_info = session.exec(Statement).all()
+    if data_info == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    #default는 배열로 json형식의 값들을 보냄
-    return deafult
+    df = []
+    for  data in data_info:
+        df.append(data.__dict__)
+    df = pd.DataFrame(df)
+    for where in wheres:
+        filtered= df[df['address'].str.contains(where)]
+    default = []
+
+    for i in range(0,len(filtered.index)):
+        which  = filtered.iloc[i].id
+        Statement = select(Store_Info).where(Store_Info.id == int(which))
+        data_info = session.exec(Statement).first()
+        default.append(data_info)
+    
+    return default
 
 
 @app.get('/store/{storeID}',response_model=Store_Info)
@@ -28,18 +42,18 @@ async def get_store(storeID:int):
     Statement = select(Store_Info).where(
         Store_Info.id == storeID
     )
-    deafult = session.exec(Statement).first()
+    default = session.exec(Statement).first()
 
-    if deafult == None:
+    if default == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    return deafult
+    return default
 
 @app.get('/store/{storeID}/menu')
 async def get_menu(storeID:int):
     Statement = select(Menu).where(
         Menu.store_id == storeID
     )
-    deafult = session.exec(Statement).all()
-    if deafult == None:
+    default = session.exec(Statement).all()
+    if default == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    return deafult
+    return default
